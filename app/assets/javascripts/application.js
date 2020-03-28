4fc556fc97d1601d5b66fb8f6e12f93e8aaf0c22
shimpei1009 <gogo.ikeike.1009@icloud.com>
2020-03-28 23:08:02 +0900

    make_log_in_front

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,8 @@ class PostsController < ApplicationController
     # binding.pry
     @post = Post.create(post_params)
     respond_to do |format|
-      format.html { redirect_to root_path  }
-      format.json 
+      format.html { redirect_to root_path }
+      format.json
     end
   end
 
